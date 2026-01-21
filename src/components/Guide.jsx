e6ef5ad749453ee3012a50e485a80b4a2a998ff0
AminTabite <210399925+AminTabite@users.noreply.github.com>
2026-01-21 17:54:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Guide.jsx
@@ -0,0 +1,51 @@
+import { Col, Row, Container, Button } from "react-bootstrap";
+import inputs from "../assets/Notation.webp";
+import { useNavigate } from "react-router-dom";
+const Guide = () => {
+  const navigate = useNavigate();
+  return (
+    <Container fluid className="px-3">
+      <Row>
+        <h2 className="my-3 text-center">
+          Here's a quick guide to Tekken's inputs!
+        </h2>
+        <Col
+          className="d-flex justify-content-center flex-grow-1"
+          xs={8}
+          lg={8}>
+          <div className="d-flex flex-column align-items-center g-4 my-4">
+            <div className="my-3 bg-body-tertiary w-100">
+              {" "}
+              <img
+                src={inputs}
+                alt="inputs guide"
+                style={{ maxHeight: "100%" }}
+                className="img-fluid"
+              />
+            </div>
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <p className="mx-2">
+                {" "}
+                HE/ H = Heat, in heat every character gets enhanced or new moves
+                available. You can activate heat by pressing 2+3!
+              </p>
+              <p className="mx-2">
+                RA = Rage art! Rage it's a state where the player has only few
+                HP left, all moves deal more damage and you get a special
+                unblockable attack! the less hp You have the more dmg It deals
+                but its -18 , be careful!{" "}
+              </p>
+            </div>
+
+            <Button
+              onClick={() => navigate("/")}
+              className="m-2 p-2 rounded-0 text-light bg-danger w-25 mx-auto">
+              Back to HomePage
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+export default Guide;
---
+++ src/components/MoveSearchbar.jsx
@@ -0,0 +1,206 @@
+import { useState } from "react";
+import { Container, Col, Row, Button, Alert, Spinner } from "react-bootstrap";
+import RosterArray from "../roster.js/arrayroster";
+import Form from "react-bootstrap/Form";
+import { FaSearch } from "react-icons/fa";
+
+const MoveSearchbar = () => {
+  const token = localStorage.getItem("token");
+  const [characterName, setCharacter] = useState("");
+  const [moveInput, setMoveinput] = useState("");
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [duplicateMoveIndex, setDuplicateMoveIndex] = useState(null);
+  const endpoint = `http://localhost:5000/proxy/tekken/${characterName}`;
+  const endpoint1 = "http://localhost:5000/favorites";
+
+  const normalizeCommand = (cmd) => {
+    if (!cmd) return "";
+    let normalized = cmd.trim().toLowerCase();
+
+    // Rimuove spazi, virgole, slash, :
+    normalized = normalized.replace(/[\s,/:]+/g, "");
+
+    // Comprime SOLO direzioni+numero pensato per mosse come 1+2/ 3+4 etc
+    normalized = normalized.replace(/([fdbu]+(?:f|b)?)\+([1234])/g, "$1$2");
+
+    return normalized;
+  };
+
+  const GetSpecificMove = async () => {
+    setError("");
+    setResult(null);
+    setLoading(true);
+
+    if (!characterName || !moveInput.trim()) {
+      setError("Select a fighter and insert an input");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const endpoint = `http://localhost:5000/proxy/tekken/${characterName}`;
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error(`Error, status: ${response.status}`);
+      }
+      const data = await response.json();
+      const frames = data.framesNormal || [];
+
+      // ← QUI usi la funzione normalizzata
+      const userquery = normalizeCommand(moveInput);
+      const found = frames.find(
+        (move) => normalizeCommand(move.command) === userquery,
+      );
+
+      if (found) {
+        setResult(found);
+      } else {
+        setError("Move not found, try again");
+      }
+    } catch (error) {
+      setError("Something went wrong! try again.");
+      console.log("Errore:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  const addToFavorites = () => {
+    if (!result || !token) return;
+
+    const payload = {
+      moveInput: result.command,
+      characterName: characterName,
+      damage: result.damage,
+      startup: result.startup,
+      onBlock: result.block,
+      onHit: result.hit,
+      hitLevel: result.hitLevel,
+      recovery: result.recovery,
+    };
+
+    const PostFavoriteMove = async (token, payload, moveIndex) => {
+      try {
+        const response = await fetch(endpoint1, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(payload),
+        });
+        if (!response.ok) {
+          if (response.status === 400) {
+            setDuplicateMoveIndex(moveIndex);
+          }
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setDuplicateMoveIndex(null);
+        console.log(data);
+      } catch (error) {
+        console.log("errore nell' aggiunta mosse", error);
+      }
+    };
+
+    PostFavoriteMove(token, payload);
+    console.log("added to favorites", payload);
+  };
+
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center mt-3 align-items-end">
+        <Col xs={5} lg={5} className="pe-2">
+          <Form.Select
+            aria-label="Select character"
+            value={characterName}
+            onChange={(e) => setCharacter(e.target.value)}
+            className="h-100">
+            <option value="">Select a character</option>
+            {RosterArray.map((char) => (
+              <option key={char.name} value={char.name}>
+                {char.name}
+              </option>
+            ))}
+          </Form.Select>
+        </Col>
+
+        <Col xs={5} lg={5} className="">
+          <Form.Control
+            type="text"
+            placeholder="(es: f,f+2 or d/f+1)."
+            value={moveInput}
+            onChange={(e) => setMoveinput(e.target.value)}
+            className="h-100"
+          />
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center ">
+          <Button
+            className="dan bg-light text-primary px-3 py-1 my-4"
+            onClick={GetSpecificMove}
+            disabled={loading}
+            size="m">
+            {loading ? (
+              <>
+                <Spinner size="sm" className="me-1" />
+                <span className="d-none d-md-inline">loading...</span>
+              </>
+            ) : (
+              <FaSearch size={16} />
+            )}
+          </Button>
+        </div>
+        {/*se e' un errore*/}
+        {error && (
+          <Alert variant="danger" className="mt-3 text-center">
+            {error}
+          </Alert>
+        )}
+        {/*se e' corretto*/}
+        {result && (
+          <div className="mt-4 p-3 border rounded bg-light text-black m-1 d-flex flex-column justify-content-between mb-3">
+            <div>
+              <strong>Command ➡️ :</strong> {result.command}
+            </div>
+            <div>
+              <strong>Damage ⚡ :</strong> {result.damage}
+            </div>
+            <div>
+              <strong>Startup 🕙 :</strong> {result.startup}
+            </div>
+            <div>
+              <strong>Block 🛡️ :</strong> {result.block}
+            </div>
+            <div>
+              <strong>Recovery ❗ :</strong> {result.recovery}
+            </div>
+            <div>
+              <strong>Hit 💥 :</strong> {result.hit}
+            </div>
+            <div>
+              <strong>Hit Level 💫 :</strong> {result.hitLevel}
+            </div>
+
+            <div
+              className="justify content center align-content-center"
+              onClick={addToFavorites}>
+              <Button className="p2 m2 bg bg-danger rounded-0">
+                Add To List
+              </Button>
+            </div>
+            {characterName && moveInput === characterName && moveInput && (
+              <div className="alert alert-danger mt-2 text-center">
+                Move already present!
+              </div>
+            )}
+          </div>
+        )}
+      </Row>
+    </Container>
+  );
+};
+
+export default MoveSearchbar;
